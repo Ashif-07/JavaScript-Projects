02533e31bd3681b86a4eb850a8e2660ef2c5eb3a
MD ASHIF ANSARI <181108726+Ashif-07@users.noreply.github.com>
2025-05-21 11:13:04 +0530

    Guessing a Number.js

--- Guessing a Number.js
+++ Guessing a Number.js
@@ -3,7 +3,7 @@ let count = 0;
 let num;
 do {
   count++;
-  num = prompt("Guess the number : ");
+  num = Number(prompt("Guess the number : "));
   if (num == rn) console.log("Number", rn, " is Guessed in ", count, " times");
   else if (rn > num)
     console.log("Original Number is greater than your number " + num + " ");
