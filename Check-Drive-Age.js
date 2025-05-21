92d68570f6652ec513106699a253bfc224bfbd78
MD ASHIF ANSARI <181108726+Ashif-07@users.noreply.github.com>
2025-05-21 13:02:37 +0530

    Check-Drive-Age.js

---
+++ Check-Drive-Age.js
@@ -0,0 +1,4 @@
+let age = Number(prompt("Enter your age : "));
+if (age >= 18 && age < 100) console.log("you can drive ");
+else if (age < 1 || age > 100) console.log("enter Valid Age");
+else console.log("You can't Drive");
