da04f0a9e7fb78b49856a51fc96410e781bd2d28
MD ASHIF ANSARI <181108726+Ashif-07@users.noreply.github.com>
2025-05-21 11:11:43 +0530

    Add files via upload

---
+++ Guessing a Number.js
@@ -0,0 +1,11 @@
+let rn = Math.floor(Math.random() * 100);
+let count = 0;
+let num;
+do {
+  count++;
+  num = prompt("Guess the number : ");
+  if (num == rn) console.log("Number", rn, " is Guessed in ", count, " times");
+  else if (rn > num)
+    console.log("Original Number is greater than your number " + num + " ");
+  else console.log("Original Number is Lesser than your number " + num + " ");
+} while (num != rn);
